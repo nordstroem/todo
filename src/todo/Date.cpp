8072792513be1fdcc2462531cb76242a4cf024fa
Johan Nordström <cjs.nordstrom@gmail.com>
2020-10-03 10:56:20 +0200

    Proper check for ill-formed date strings

--- src/todo/Date.cpp
+++ src/todo/Date.cpp
@@ -1,8 +1,22 @@
 #include "Date.hpp"
+#include <algorithm>
+#include <array>
 #include <date/date.h>
 #include <fmt/core.h>
 
 namespace todo {
+
+static bool validDateString(const std::string& string)
+{
+    if (string.length() != 10)
+        return false;
+    constexpr auto digitPositions = std::array<int, 8>{0, 1, 2, 3, 5, 6, 8, 9};
+    if (std::any_of(digitPositions.begin(), digitPositions.end(), [&](const auto& p) { return !std::isdigit(string.at(p)); }))
+        return false;
+    if (string.at(4) != '-' || string.at(7) != '-')
+        return false;
+    return true;
+}
 
 Date Date::today()
 {
@@ -15,12 +29,12 @@ std::optional<Date> Date::fromString(const std::string& string)
     if (string == "today")
         return Date::today();
 
-    try {
+    if (validDateString(string)) {
         int year = std::stoi(string.substr(0, 4));
         unsigned month = std::stoull(string.substr(5, 2));
         unsigned day = std::stoull(string.substr(8, 2));
         return Date{.year = year, .month = month, .day = day};
-    } catch (const std::exception& e) {
+    } else {
         return std::nullopt;
     }
 }
