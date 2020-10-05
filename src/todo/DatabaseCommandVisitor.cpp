5fa07d8b9a7bfaf133aacc47cb1104a10085dcf6
Johan Nordström <cjs.nordstrom@gmail.com>
2020-10-05 19:51:51 +0200

    Improve show output

--- src/todo/DatabaseCommandVisitor.cpp
+++ src/todo/DatabaseCommandVisitor.cpp
@@ -19,10 +19,22 @@ void DatabaseCommandVisitor::operator()(ShowTasks&& cmd) const
 {
     const auto& tasks = this->_database.at(cmd.date);
     if (tasks.size() > 0) {
-        auto maxLength = std::max_element(tasks.begin(), tasks.end(), [](const auto& a, const auto& b) { return a.description.length() < b.description.length(); })->description.size();
+        auto maxHash = std::to_string(std::max_element(tasks.begin(), tasks.end(), [](const auto& a, const auto& b) { return std::to_string(a.hash).length() < std::to_string(b.hash).length(); })->hash).length();
+        auto maxDescription = std::max_element(tasks.begin(), tasks.end(), [](const auto& a, const auto& b) { return a.description.length() < b.description.length(); })->description.size();
+        auto maxPriority = std::to_string(std::max_element(tasks.begin(), tasks.end(), [](const auto& a, const auto& b) { return std::to_string(a.priority).length() < std::to_string(b.priority).length(); })->priority).length();
+
+        auto hashString = std::string("Hash");
+        int hashPadding = std::max(maxHash, hashString.length()) + 1;
+        auto descriptionString = std::string("Description");
+        int descriptionPadding = std::max(maxDescription, descriptionString.length());
+        auto priorityString = std::string("Priority");
+        int priorityPadding = std::max(maxPriority, priorityString.length());
+        auto doneString = std::string("Done");
+
+        fmt::print("{:>{}}  {:<{}}  {:<{}}  {}\n", hashString, hashPadding, descriptionString, descriptionPadding, priorityString, priorityPadding, doneString);
         for (const auto& task : tasks) {
             auto checked = task.done ? fmt::format(fg(fmt::color::green), "V") : fmt::format(" ");
-            fmt::print("{:<{}} [{}] {} {:x}\n", task.description, maxLength + 5, checked, task.priority, task.hash);
+            fmt::print("{:>{}}  {:<{}}  {:<{}}  [{}]\n", task.hash, hashPadding, task.description, descriptionPadding, task.priority, priorityPadding, checked);
         }
     } else {
         fmt::print("Nothing to do {}\n", cmd.date == Date::today() ? "today" : "at this date");
